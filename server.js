030a1d824ba23f6749a61e76ab0f321210860737
grynka <ihor.kozhemyakin@gmail.com>
2023-02-23 19:33:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.set('strictQuery', false)
+
+const PORT = 3000;
+const HOST = 'mongodb+srv://grynka:230888@cluster0.s6bvtdk.mongodb.net/contacts';
+
+const connection = mongoose.connect(HOST, {
+  promiseLibrary: global.Promise,
 })
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log("Database connection successful")
+    })
+  })
+  .catch((err) =>{
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1)}
+  )
